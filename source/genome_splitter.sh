31c8d2f8cd660d6ac2ef3b6acde7c9e54e5da6b7
= <samw430@gmail.com>
2020-05-01 20:41:56 -0400

    utility script

---
+++ source/genome_splitter.sh
@@ -0,0 +1,10 @@
+head -83470 >> 10_percent_human.fasta
+head -167479 >> 20_percent_human.fasta
+head -251219 >> 30_percent_human.fasta
+head -334959 >> 40_percent_human.fasta
+head -418699 >> 50_percent_human.fasta
+head -502438 >> 60_percent_human.fasta
+head -586179 >> 70_percent_human.fasta
+head -669918 >> 80_percent_human.fasta
+head -753657 >> 90_percent_human.fasta
+
