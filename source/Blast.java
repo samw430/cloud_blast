728c420734e918d53aa3cc5c040776d410f72c0c
= <samw430@gmail.com>
2020-05-03 11:50:44 -0400

    Updates

--- source/Blast.java
+++ source/Blast.java
@@ -94,8 +94,8 @@ public static class OffSetMapper extends Mapper < LongWritable, Text,
         	System.out.println("I'm setting up");
 
         	FileSystem fs = FileSystem.get(context.getConfiguration()); 
-        	System.out.println(new Path("query"));
-        	BufferedReader reader = new BufferedReader(new InputStreamReader(fs.open(new Path("query")))); 
+        	Path query_path = new Path("hdfs://172.31.57.12:9000/user/ubuntu/query");
+        	BufferedReader reader = new BufferedReader(new InputStreamReader(fs.open(query_path))); 
 			String query = "";	
 			String line = "";			
 		    while ((line = reader.readLine()) != null){ 
